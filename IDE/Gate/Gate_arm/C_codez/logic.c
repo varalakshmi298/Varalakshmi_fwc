22ae15b8a5a73a01cdd1d1826c1b47c1edd2c384
Nissankara Varalakshmi <varalakshminissankara4@gmail.com>
2025-07-10 11:28:49 +0530

    Add files via upload

---
+++ IDE/Gate/Gate_arm/C_codez/logic.c
@@ -0,0 +1,7 @@
+#include "logic.h"
+
+// Computes f = !(P OR Q OR R)
+int compute_logic(int P, int Q, int R) {
+    return !(P || Q || R);
+}
+
---
+++ IDE/Gate/Gate_arm/C_codez/logic.h
@@ -0,0 +1,8 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+
+// Function prototype for f = !(P OR Q OR R)
+int compute_logic(int P, int Q, int R);
+
+#endif
+
